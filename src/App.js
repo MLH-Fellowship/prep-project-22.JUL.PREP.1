84fd3b8f6bb2d8ca04fd66d40c0093ff06e4622b
merouanezouaid <marwan.zouaid@gmail.com>
2022-07-19 14:42:21 +0100

    get user location and city

--- src/App.js
+++ src/App.js
@@ -9,6 +9,42 @@ function App() {
   const [results, setResults] = useState(null);
 
   useEffect(() => {
+
+    function onSuccess(position) {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setCity(result.name);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+    }
+
+    function onError(error) {
+      setError(error);
+    }
+
+    if(!navigator.geolocation) {
+      alert('Geolocation is not supported by your browser');
+    } else {
+    navigator.geolocation.getCurrentPosition(
+      onSuccess,
+      onError
+    );
+    }
+  }, []);
+
+
+  useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
       .then(
@@ -40,7 +76,6 @@ function App() {
           onChange={event => setCity(event.target.value)} />
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
-          {console.log(results)}
           {isLoaded && results && <>
             <h3>{results.weather[0].main}</h3>
             <p>Feels like {results.main.feels_like}°C</p>
--- src/index.js
+++ src/index.js
@@ -9,27 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-function findLoc() {
-  
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    alert(`Latitude: ${latitude}°, Longitude: ${longitude}°`);
-  }
-
-  function error() {
-    alert('Unable to retrieve your location');
-  }
-
-  if(!navigator.geolocation) {
-    alert('Geolocation is not supported by your browser');
-  } else {
-    alert('Locating…');
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-}
-
-
-document.addEventListener("DOMContentLoaded", findLoc);
