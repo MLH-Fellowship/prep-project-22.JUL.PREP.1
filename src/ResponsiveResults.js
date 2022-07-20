6d58dfab353d28bc811e5d4766e301ed746a58d6
akshitagupta15june <akshitagupta15june@gmail.com>
2022-07-20 19:51:05 +0530

    added aspect ration and yarn

--- src/ResponsiveResults.js
+++ src/ResponsiveResults.js
@@ -12,7 +12,18 @@ const Mobile = ({ children }) => {
     const isMobile = useMediaQuery({ maxWidth: 767 })
     return isMobile ? children : null
 }
-
+const Iphone12mini = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 360 })
+    return isMobile ? children : null
+}
+const Iphone12pro = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 390 })
+    return isMobile ? children : null
+}
+const Iphone11 = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 414 })
+    return isMobile ? children : null
+}
 
 const ResponsiveResults = ({weather, feelsLike, country, place}) => (
     <div>
@@ -37,6 +48,27 @@ const ResponsiveResults = ({weather, feelsLike, country, place}) => (
                 <i><p>{place}, {country}</p></i>
             </div>
         </Mobile>
+        <Iphone12mini>
+            <div className="Iphone12mini">
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </Iphone12mini>
+        <Iphone12pro>
+            <div className="Iphone12pro">
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i> 
+            </div>
+        </Iphone12pro>
+        <Iphone11>
+            <div className="Iphone11">
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </Iphone11>
     </div>
 )
 
