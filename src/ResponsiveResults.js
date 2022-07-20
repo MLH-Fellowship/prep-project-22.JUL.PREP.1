2391a674733d8937905ab16ef53885cc66c2f857
akshitagupta15june <akshitagupta15june@gmail.com>
2022-07-20 17:26:48 +0530

    run site

--- src/ResponsiveResults.js
+++ src/ResponsiveResults.js
@@ -13,6 +13,26 @@ const Mobile = ({ children }) => {
     return isMobile ? children : null
 }
 
+const iphone12mini = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 360 })
+    return isMobile ? children : null
+}
+const iphone12pro = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 390 })
+    return isMobile ? children : null
+}
+const iphone11 = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 414 })
+    return isMobile ? children : null
+}
+const oneplus6 = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 412 })
+    return isMobile ? children : null
+}
+const extralargescreen = ({ children }) => {
+    const isMobile = useMediaQuery({ minWidth: 1201 })
+    return isMobile ? children : null
+}
 
 const ResponsiveResults = ({weather, feelsLike, country, place}) => (
     <div>
@@ -37,7 +57,44 @@ const ResponsiveResults = ({weather, feelsLike, country, place}) => (
                 <i><p>{place}, {country}</p></i>
             </div>
         </Mobile>
+        <iphone12mini>
+            <div className="iphone12mini">
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </iphone12mini>
+        <iphone12pro>
+            <div className='iphone12pro'>
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i> 
+            </div>
+        </iphone12pro>
+        <iphone11>
+            <div className='iphone11'>
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </iphone11>
+        <oneplus6>
+            <div className='oneplus6'>
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </oneplus6>
+        <extralargescreen>
+            <div className='extralargescreen'>
+                <h3>{weather}</h3>
+                <p>Feels like {feelsLike}°C</p>
+                <i><p>{place}, {country}</p></i>
+            </div>
+        </extralargescreen>
     </div>
+
+    
 )
 
 export default ResponsiveResults
